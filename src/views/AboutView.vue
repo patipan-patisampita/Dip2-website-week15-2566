7d0faa039280ca5824df3a8b273c0ef68dce5eda
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-23 12:52:00 +0700

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
+<script setup>
+
+</script>
+
 <template>
-  <div>
-    <h1>This is an about page</h1>
+  <div class="container my-5">
+    <div class="card shadow" style="width: 20rem;">
+      <div class="card-header p-0">
+        <img src="https://images.adsttc.com/media/images/5a2a/d784/b22e/3819/db00/021f/slideshow/BeFunky_Collage.jpg?1512757113" class="card-img-top"
+          alt="img">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">About Page</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Read...</a>
+      </div>
+    </div>
   </div>
 </template>

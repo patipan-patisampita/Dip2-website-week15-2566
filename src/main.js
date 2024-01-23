55bae46e78a49034c1a5c60ff22895b5ccf47646
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-23 12:10:07 +0700

    update

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 const app = createApp(App)
 

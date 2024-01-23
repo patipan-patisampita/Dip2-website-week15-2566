73c4eef278dcf455231db58fbbc6b13a1fa73539
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-23 12:20:29 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from './views/layouts/NavBar.vue'
+import FooterView from './views/layouts/FooterView.vue'
 </script>
 
 <template>
   <nav-bar />
   <RouterView />
+  <FooterView />
 </template>
 
 <style scoped>

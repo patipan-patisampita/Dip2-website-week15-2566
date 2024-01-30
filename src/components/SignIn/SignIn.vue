008fa72d483aeb6374c19f22a028fa20b35ecef1
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-30 15:04:33 +0700

    update

--- src/components/SignIn/SignIn.vue
+++ src/components/SignIn/SignIn.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { reactive } from 'vue';
+const userForm = reactive({
+    email: "",
+    password: "",
+});
+
+</script>
+
 <template>
     <div class="container mt-4 mb-5">
         <div class="row">
@@ -7,16 +16,26 @@
                         <h5 class="text-white">เข้าสู่ระบบการจัดการออนไลน์</h5>
                     </div>
                     <div class="card-body text-success">
-                        xx
+                        <form>
+                            <div class="mb-2">
+                                <label for="" class="form-label">อีเมลล์</label>
+                                <input type="email" class="form-control" v-model="userForm.email"
+                                    placeholder="อีเมลล์ผู้สมัคร" required>
+                            </div>
+                            <div class="mb-2">
+                                <label for="" class="form-label">รหัสผ่าน</label>
+                                <input type="password" class="form-control" v-model="userForm.password"
+                                    placeholder="รหัสผ่านผู้สมัคร" required>
+                            </div>
+                            <div class="mb-2">
+                                <p type="submit" class="btn btn-primary shadow d-block">สมัครสมาชิก</p>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
-<script setup>
-
-</script>
 
 <style lang="scss" scoped></style>

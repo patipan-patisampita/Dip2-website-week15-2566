4ccab8ab19abf8ff8203978a03bbfb64228274c6
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-30 14:57:47 +0700

    update

--- src/components/SignIn/SignIn.vue
+++ src/components/SignIn/SignIn.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        <h1>Sign In form</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="card shadow">
+                    <div class="card-header bg-primary">
+                        <h5 class="text-white">เข้าสู่ระบบการจัดการออนไลน์</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,6 +16,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

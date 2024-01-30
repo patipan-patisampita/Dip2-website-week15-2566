bf52cda1b754bd96ac826f8c54e404bc4c660319
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-30 11:54:12 +0700

    update

--- src/components/SingUp.vue/SignUp.vue
+++ src/components/SingUp.vue/SignUp.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        <h1>Sign Up Form</h1>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-5 mb-1">
+                <div class="card shadow">
+                    <div class="card-header p-0">
+                        <img src="https://cdn.pixabay.com/photo/2016/08/03/13/33/stone-henge-1566686_640.jpg" class="card-img-top" alt="sign">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text text-end">สิ่งมหัศรรย์ของโลก 2024.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="card shadow">
+
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,6 +24,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

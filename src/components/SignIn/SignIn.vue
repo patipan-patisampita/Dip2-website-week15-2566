56033c50a4ef603dc8dea439a103f6fa74ea08a4
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-30 15:01:17 +0700

    update

--- src/components/SignIn/SignIn.vue
+++ src/components/SignIn/SignIn.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="container">
+    <div class="container mt-4 mb-5">
         <div class="row">
-            <div class="col-md-5">
+            <div class="col-md-5 mx-auto">
                 <div class="card shadow">
                     <div class="card-header bg-primary">
                         <h5 class="text-white">เข้าสู่ระบบการจัดการออนไลน์</h5>
+                    </div>
+                    <div class="card-body text-success">
+                        xx
                     </div>
                 </div>
             </div>

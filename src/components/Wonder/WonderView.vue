2aff01671e90b95e4dd727de84b205f47d88bba7
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-06 14:29:50 +0700

    update

--- src/components/Wonder/WonderView.vue
+++ src/components/Wonder/WonderView.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        <h1>Wondes of the world</h1>
+    <div class="container mt-4">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,12 +22,10 @@ import { onMounted } from 'vue';
 onMounted(() => {
     axios.get(`http://localhost:3000/wonders`)
         .then((response) => {
-        console.log(response.data)
-    })
+            console.log(response.data)
+        })
 })
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

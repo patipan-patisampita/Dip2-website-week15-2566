7123e15b76beaa231b747c9917b4023a475394c3
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-06 12:09:08 +0700

    update

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        <h2>Error Page</h2>
+    <div class="container mt-4">
+        <div class="d-flex">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header p-0">
+                            <img src="https://static.vecteezy.com/system/resources/previews/006/549/647/non_2x/404-landing-page-free-vector.jpg"
+                                class="card-img-top" alt="NotFound">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,6 +19,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
